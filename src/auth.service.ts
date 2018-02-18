691c7b9f0cacb107dada7b22b525fa11535cd9b5
amanecer <shahar@sportstraders.io>
2018-02-18 15:22:41 +0200

    direction fix

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Observer } from "rxjs/Observer";
-import { saveToStorage, loadFromStorage, deleteFromStorage} from "storage.utils";
+import { saveToStorage, loadFromStorage, deleteFromStorage} from "./storage.utils";
 
 declare let gapi: any;
 declare let IN: any;
